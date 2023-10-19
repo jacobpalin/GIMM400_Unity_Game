6e26e78f5af7919dff427302a6b518a0412296bf
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2023-10-19 10:06:44 -0600

    messing with health
    health currently reduced on frame lag (not good) and does not read collisions properly with other player only self

--- GIMM400_Unity_GroupGame/Assets/Scripts/PlayerHealth.cs
+++ GIMM400_Unity_GroupGame/Assets/Scripts/PlayerHealth.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
-    public float health;
+    public float playerHealth;
     public float maxHealth;
-    public Image healthBar;
-    public GameObject UI;
+    //public Image healthBar;
+    //public GameObject UI;
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = health;
+        playerHealth = 3.0f;
+        //maxHealth = health;
         //UI = Instantiate(UI, transform.position, transform.rotation);
-        GameObject healthBar = GameObject.FindGameObjectWithTag("Player1Health");
+        //GameObject healthBar = GameObject.FindGameObjectWithTag("Player1Health");
        //if(imageObject != null)
        // {
          //   healthBar = imageObject.GetComponent<imageObject>();
@@ -23,6 +24,13 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 3);   
+        //healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 3);
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerHealth -=1;
+        }
     }
 }
